eb46df301f6eec4d8e55e831312fe19cc806d4b5
manueldiamond <manueldiamondlistowell@gmail.com>
2024-05-13 20:48:41 +0000

    Updated for Arjun on upwork

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Navbar from './components/Navbar';
+
+export default function Home() {
+  return (
+ 
+    <main className="container mx-auto mt-8 p-4">
+      <div className="bg-gray-900 text-white">
+      <div className="container mx-auto px-4 py-12">
+        <div className="flex flex-col items-center justify-center text-center">
+          <h1 className="text-4xl lg:text-6xl font-bold mb-4">Fully Responsive Navbar</h1>
+          <p className="text-lg lg:text-xl mb-8">This is the Navbar, and it works just like <a href="https://www.whatsnxt.in" target="_blank" rel="noopener noreferrer" className="underline hover:text-blue-400">WhatsNXT</a>. I would love to help you implement this and lots more.</p>
+          <p>See you on upwork</p>
+          <a href="https://www.upwork.com/jobs/~01a6f3d354761583c6" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out">Get Started</a>
+        </div>
+      </div>
+    </div>
+    
+  </main>
+
+  );
+}
